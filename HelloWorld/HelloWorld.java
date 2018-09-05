f6d2e13ac0966f23b3f7b3484c07df6e6ca7484c
isakemling <43010088+isakemling@users.noreply.github.com>
2018-09-05 13:09:26 -0500

    Part 1 of 1.1.1

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author Isabella Kemling
+ * @version 09/05/2018
+ */
+public class HelloWorld
+{
+    public static void main ()
+       {
+           System.out.println("Hello World!");  
+        }
+       
+    }
+
+  
